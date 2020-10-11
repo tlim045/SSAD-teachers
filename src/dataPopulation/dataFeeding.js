509b634abaaa79a9ead1db03029ae35202c4ce4e
koapham <47772920+koapham@users.noreply.github.com>
2020-10-12 02:08:46 +0800

    data populating

---
+++ src/dataPopulation/dataFeeding.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+axios.defaults.baseURL = "http://localhost:3000/";
+
+const { questionBank } = require('./testData');
+
+for(var i = 0; i < questionBank.length; i++){
+    const data = {body: questionBank[i]}
+    setTimeout(function(){
+        axios.post('/createQuestion', data)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    },i * 2000)
+}
---
+++ src/dataPopulation/testData.js
@@ -0,0 +1,158 @@
+exports.questionBank = [
+    // {
+    //     "Options": {
+    //         "a": "Function point sizing",
+    //         "b": "Change sizing",
+    //         "c": "Standard component sizing",
+    //         "d": "Fuzzy logic sizing"
+    //     },
+    //     "Question": "Which software project sizing approach develop estimates of the information domain characteristics?",
+    //     "Type": "MCQ",
+    //     "CorrectAns": 1,
+    //     "Chapter": "Planning and Defining",
+    //     "Topic": "Decomposition Techniques",
+    //     "Difficulty": "Easy", 
+    // },
+    {
+        "Options": {
+            "a": "estimation",
+            "b": "system",
+            "c": "problem",
+            "d": "logic"
+        },
+        "Question": "Cost and effort estimation of a software uses only one forms of decomposition, either decomposition of the ___________ or decomposition of the process.",
+        "Type": "MCQ",
+        "CorrectAns": 3,
+        "Chapter": "Planning and Defining",
+        "Topic": "Decomposition Techniques",
+        "Difficulty": "Easy", 
+    },
+    {
+        "Options": {
+            "a": "logic",
+            "b": "estimation",
+            "c": "system",
+            "d": "process"
+        },
+        "Question": "Cost and effort estimation of a software uses only one forms of decomposition, either decomposition of the problem or decomposition of the __________.",
+        "Type": "MCQ",
+        "CorrectAns": 4,
+        "Chapter": "Planning and Defining",
+        "Topic": "Decomposition Techniques",
+        "Difficulty": "Easy", 
+    },
+    {
+        "Options": {
+            "a": "decomposition",
+            "b": "estimation",
+            "c": "logic",
+            "d": "problem"
+        },
+        "Question": "Cost and effort estimation of a software uses only one forms of decomposition, either _____________ of the problem or decomposition of the process.",
+        "Type": "MCQ",
+        "CorrectAns": 1,
+        "Chapter": "Planning and Defining",
+        "Topic": "Decomposition Techniques",
+        "Difficulty": "Easy", 
+    },
+    {
+        "Options": {
+            "a": "True",
+            "b": "False",
+            "c": "Not given",
+            "d": "Cannot tell"
+        },
+        "Question": "In many cases, it is often more cost-effective to acquire, rather than develop, computer software.",
+        "Type": "MCQ",
+        "CorrectAns": 1,
+        "Chapter": "Planning and Defining",
+        "Topic": "Decomposition Techniques",
+        "Difficulty": "Medium", 
+    },
+    {
+        "Options": {
+            "a": "The software may be purchased off-the-shelf",
+            "b": "“Full-experience” or “Partial-experience” software components should be used",
+            "c": "Customer-built software should be developed",
+            "d": "All of the mentioned"
+        },
+        "Question": "A make-buy decision is based on whether",
+        "Type": "MCQ",
+        "CorrectAns": 4,
+        "Chapter": "Planning and Defining",
+        "Topic": "Decomposition Techniques",
+        "Difficulty": "Medium", 
+    },
+    {
+        "Options": {
+            "a": "External inputs",
+            "b": "External outputs",
+            "c": "External process",
+            "d": "External inquiries"
+        },
+        "Question": "Which of the following is not one of the five information domain characteristics of Function Point (FP) decomposition?",
+        "Type": "MCQ",
+        "CorrectAns": 3,
+        "Chapter": "Planning and Defining",
+        "Topic": "Decomposition Techniques",
+        "Difficulty": "Medium", 
+    },
+    {
+        "Options": {
+            "a": "Personnel Factor",
+            "b": "Product Factor",
+            "c": "Platform Factor",
+            "d": "Project Factor"
+        },
+        "Question": "Programming language experience is a part of which factor of COCOMO cost drivers?",
+        "Type": "MCQ",
+        "CorrectAns": 1,
+        "Chapter": "Planning and Defining",
+        "Topic": "Decomposition Techniques",
+        "Difficulty": "Medium", 
+    },
+    {
+        "Options": {
+            "a": "LOC",
+            "b": "FP",
+            "c": "Fuzzy Logic",
+            "d": "LOC and FP"
+        },
+        "Question": "If an Indirect approach is taken, then the sizing approach is represented as",
+        "Type": "MCQ",
+        "CorrectAns": 2,
+        "Chapter": "Planning and Defining",
+        "Topic": "Decomposition Techniques",
+        "Difficulty": "Medium", 
+    },
+    {
+        "Options": {
+            "a": "indirect",
+            "b": "direct",
+            "c": "immediate",
+            "d": "decomposition"
+        },
+        "Question": "If a/an ______________ approach is taken, then the sizing approach is represented as Function Point",
+        "Type": "MCQ",
+        "CorrectAns": 1,
+        "Chapter": "Planning and Defining",
+        "Topic": "Decomposition Techniques",
+        "Difficulty": "Medium", 
+    }
+]
+
+
+    // {
+    //     "Options": {
+    //         "a": "LOC",
+    //         "b": "FP",
+    //         "c": "LOC and FP",
+    //         "d": "None of the mentioned"
+    //     },
+    //     "Question": "If a Direct approach to software project sizing is taken, size can be measured in",
+    //     "Type": "MCQ",
+    //     "CorrectAns": 1,
+    //     "Chapter": "Planning and Defining",
+    //     "Topic": "Decomposition Techniques",
+    //     "Difficulty": "Easy", 
+    // },
